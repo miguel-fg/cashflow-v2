1407d78c9c1fe8a0cb0503ab29675048def368df
miguel-fg <miguelfierro55@gmail.com>
2024-06-29 12:13:56 -0700

    Add db initialization

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,15 @@
+import React, { useEffect } from "react";
 import { Stack } from "expo-router/stack";
+import { createTables } from "../db/database";
 
 export default function RootLayout() {
+  useEffect(() => {
+    const initializeDatabase = async () => {
+      await createTables();
+    };
+    initializeDatabase();
+  }, []);
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
--- db/database.js
+++ db/database.js
@@ -1,12 +1,48 @@
-import PouchDB from "pouchdb-react-native";
-import PouchDBFind from "pouchdb-find";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as SQLite from "expo-sqlite";
 
-PouchDB.plugin(PouchDBFind);
+let db;
 
-PouchDB.plugin(require("pouchdb-adapter-asyncstorage").default);
+const initDB = async () => {
+  if (!db) {
+    db = await SQLite.openDatabaseAsync("app.db");
+  }
+  return db;
+};
 
-// DB init
-const db = new PouchDB("cashflow", { adapter: "asyncstorage" });
+const createTables = async () => {
+  try {
+    const db = await initDB();
+    await db.execAsync(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL
+      );
+      CREATE TABLE IF NOT EXISTS accounts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        currency TEXT,
+        type TEXT,
+        amount REAL DEFAULT 0,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+      );
+      CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        account_id INTEGER,
+        category TEXT,
+        description TEXT,
+        amount REAL,
+        type TEXT,
+        date TEXT,
+        FOREIGN KEY (account_id) REFERENCES accounts(id)
+      );
+    `);
 
-export default db;
+    console.log("Tables created successfully");
+  } catch (error) {
+    console.log("Error creating tables: ", error);
+  }
+};
+
+export { initDB, createTables };
